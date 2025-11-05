e06d7d97167b4496fe887a7a9c72771119626fd8
ShawkyXr <ahmedshawkyx8@gmail.com>
2025-11-05 21:52:27 +0200

    link database and routes

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express, { json } from "express";
+import { connect } from "mongoose";
+import { config } from "dotenv";
 
-dotenv.config();
+import urlRoutes from './Routes/url.route.js';
+
+
+config();
 
 const app = express();
+
+app.use(json());
+
 const PORT = process.env.PORT || 3000;
+
+connect(process.env.MONGO_URL)
+.then(() => console.log("MongoDB connected"))
+.catch((error) => {
+  console.error("MongoDB connection error:", error);
+  process.exit(1);
+});
+
+
+app.use(json());
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+app.use('/blink', urlRoutes);
+
 
 
 app.listen(PORT, () => {
