f61677de81f5af6c67828d94e9583611a44cbbc0
ShawkyXr <ahmedshawkyx8@gmail.com>
2025-11-06 01:16:31 +0200

    add user property

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 import { config } from "dotenv";
 
 import urlRoutes from './Routes/url.route.js';
-
+import userRoutes from './Routes/user.route.js';
 
 config();
 
@@ -14,17 +14,16 @@ app.use(json());
 const PORT = process.env.PORT || 3000;
 
 connect(process.env.MONGO_URL)
-.then(() => console.log("MongoDB connected"))
-.catch((error) => {
-  console.error("MongoDB connection error:", error);
-  process.exit(1);
-});
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  });
 
 
 app.use(json());
-app.get('/favicon.ico', (req, res) => res.status(204).end());
 app.use('/blink', urlRoutes);
-
+app.use('/blink/user', userRoutes);
 
 
 app.listen(PORT, () => {
